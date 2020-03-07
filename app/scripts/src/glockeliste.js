aa0f40eec4522435269f9c2d10907417ea185c6f
Massimo Mazza <massimo.mazza01@gmail.com>
2020-03-07 19:48:11 +0100

    Glocke 068 wieder aktiviert

--- app/scripts/src/glockeliste.js
+++ app/scripts/src/glockeliste.js
@@ -16,7 +16,7 @@ var glocke = new function() {
         { "Sattel": "087", "Glocke": "064", "Bem": "1 rosa" },
         // { "Sattel": "066", "Glocke": "066", "Bem": "1 rosa" },
         { "Sattel": "078", "Glocke": "099", "Bem": "1 rosa" },
-        { "Sattel": "067", "Glocke": "067", "Bem": "1 rosa" },
+        { "Sattel": "067", "Glocke": "068", "Bem": "1 rosa" },
         { "Sattel": "0100", "Glocke": "032", "Bem": "1 rosa" },
         { "Sattel": "0106", "Glocke": "0107", "Bem": "1 rosa" },
         // { "Sattel": "090", "Glocke": "0104", "Bem": "2 rot" },
